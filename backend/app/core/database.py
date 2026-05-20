eccdb3f43d3997f3aec0632d385feae62671f15a
FredMsv <fredericmsv@gmail.com>
2026-05-20 15:53:24 +0200

    fix: add event listener to disable prepared statements for pgbouncer

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,6 +1,7 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import event
 from app.core.config import settings
 
 engine = create_async_engine(
@@ -10,16 +11,18 @@ engine = create_async_engine(
     connect_args={"statement_cache_size": 0},
 )
 
+@event.listens_for(engine.sync_engine, "connect")
+def connect(dbapi_connection, connection_record):
+    dbapi_connection._connection._protocol.queries_count = 0
+
 AsyncSessionLocal = async_sessionmaker(
     engine,
     class_=AsyncSession,
     expire_on_commit=False,
 )
 
-
 class Base(DeclarativeBase):
     pass
-
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
